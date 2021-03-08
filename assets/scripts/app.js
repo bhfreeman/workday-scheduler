85a5e7a8cbc2903ac0cb0d6555dc19490503d772
bhfreeman <brett.h.freeman@gmail.com>
2021-03-08 15:05:56 -0700

    added functionality to style based on current time

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,25 +1,9 @@
-
 //Display the current day at the top of the calendar
 $("#currentDay").text(moment().format("dddd, MMMM Do YYYY"));
 
 //Make timeblocks for each standard business hour
 for (let i = 9; i < 18; i++) {
-    // var container = $(".container");
-    // var timeBlock = $("<div>");
-    // var hourBlock = $("<div>");
-    // var textBlock = $("<textarea>");
-    // var saveBlock = $("<button>");
-    
-    // timeBlock.addClass("row time-block");
-    // hourBlock.addClass("hour col-md-1");
-    // textBlock.addClass("col-md-10 description");
-    // saveBlock
-    //   .addClass("btn saveBtn col-md-1")
-    //   .append($("<i>").attr("class", "fas fa-save"));
-    
-    // timeBlock.append(hourBlock).append(textBlock).append(saveBlock);
-    // container.append(timeBlock);
-    createTimeBlock(i);
+  createTimeBlock(i);
 }
 
 //Color code the timeblocks based on past present future
@@ -29,19 +13,27 @@ for (let i = 9; i < 18; i++) {
 //There is a save button in the timeblock that will save the text in the timeblock into local storage so that the item persists through reloads
 
 function createTimeBlock(i) {
-    var container = $(".container");
-    var timeBlock = $("<div>");
-    var hourBlock = $("<div>");
-    var textBlock = $("<textarea>");
-    var saveBlock = $("<button>");
-    
-    timeBlock.addClass("row time-block");
-    hourBlock.addClass("hour col-md-1").append(i);
-    textBlock.addClass("col-md-10 description");
-    saveBlock
-      .addClass("btn saveBtn col-md-1")
-      .append($("<i>").attr("class", "fas fa-save"));
-    
-    timeBlock.append(hourBlock).append(textBlock).append(saveBlock);
-    container.append(timeBlock);
+  var container = $(".container");
+  var timeBlock = $("<div>");
+  var hourBlock = $("<div>");
+  var textBlock = $("<textarea>");
+  var saveBlock = $("<button>");
+
+  timeBlock.addClass("row time-block");
+  hourBlock.addClass("hour col-md-1").append(i);
+  textBlock.addClass("col-md-10 description").attr("id", i);
+  saveBlock
+    .addClass("btn saveBtn col-md-1")
+    .append($("<i>").attr("class", "fas fa-save"));
+
+  timeBlock.append(hourBlock).append(textBlock).append(saveBlock);
+  container.append(timeBlock);
+
+  if (parseInt(moment().format("H")) === i) {
+    textBlock.addClass("present");
+  } else if (parseInt(moment().format("H")) > i) {
+    textBlock.addClass("past");
+  } else {
+    textBlock.addClass("future");
+  }
 }
